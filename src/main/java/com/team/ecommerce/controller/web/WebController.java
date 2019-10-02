834018fcb7a9d77698215bc0ac124a8e4e6b5a4f
e-Commerce <hstdra@gmail.com>
2019-10-02 10:01:58 +0700

    v0.0.2 - Fix somethings

--- src/main/java/com/team/ecommerce/controller/web/WebController.java
+++ src/main/java/com/team/ecommerce/controller/web/WebController.java
@@ -4,8 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.persistence.EntityManager;
-
 @Controller
 @RequestMapping("/web")
 public class WebController {
