dd3cd4b6c9e44f135cf755da6ac158309c960a99
newCityHunter <pbt.anh19992@gmail.com>
2019-11-05 08:24:51 +0700

    Loz Han version 1

--- src/main/java/com/team/ecommerce/controller/admin/ProductController.java
+++ src/main/java/com/team/ecommerce/controller/admin/ProductController.java
@@ -1,5 +1,7 @@
 package com.team.ecommerce.controller.admin;
 
+import com.team.ecommerce.entity.Category;
+import com.team.ecommerce.entity.FieldDetail;
 import com.team.ecommerce.entity.Product;
 import com.team.ecommerce.service.CategoryService;
 import com.team.ecommerce.service.HistotyService;
@@ -8,10 +10,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
 
 @Controller
 @RequestMapping("admin/product")
@@ -49,6 +52,46 @@ public class ProductController {
         try {
             productService.save(product);
         } catch (Exception ignored) {
+        }
+        return "redirect:/admin/product";
+    }
+
+    // Import new product //
+    @RequestMapping("chooseCategory")
+    public String listAllCategory(Model model) {
+        model.addAttribute("categories", categoryService.listAll());
+        return "admin/product/chooseCategory";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String addProduct(Model model, @RequestParam("getCategoryId") Integer categoryId) {
+        Product product = new Product();
+        Category category = categoryService.getOne(categoryId);
+        product.setCategory(category);
+        product.setFieldDetails(new LinkedList<>());
+        category.getFields().forEach(f -> {
+            FieldDetail fieldDetail = new FieldDetail();
+            fieldDetail.setField(f);
+            product.getFieldDetails().add(fieldDetail);
+        });
+
+        model.addAttribute("fields", category.getFields());
+        model.addAttribute("product", product);
+        return "admin/product/add";
+    }
+
+    @RequestMapping(value = "saveAdd", method = RequestMethod.POST)
+    public String save(@ModelAttribute Product product, @RequestParam String fds) {
+        try {
+            Map<String, String> fieldDetails = new HashMap<>();
+            String[] details = fds.split(";;;");
+            Category category = categoryService.getOne(product.getCategory().getId());
+            for (int i = 0; i < category.getFields().size(); i++) {
+                fieldDetails.put(category.getFields().get(i).getField(), details[i]);
+            }
+            productService.saveProduct(product.getName(), product.getCategory().getCategory(), product.getPrice(), product.getDiscount(), 0, null, product.getDescription(), fieldDetails);
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
         return "redirect:/admin/product";
     }
