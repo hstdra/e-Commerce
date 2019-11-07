bd75760d47b7d43c54421888fb93466f41729e48
e-Commerce <hstdra@gmail.com>
2019-11-07 10:14:54 +0700

    Fix n bugs

--- src/main/java/com/team/ecommerce/repository/OrderRepository.java
+++ src/main/java/com/team/ecommerce/repository/OrderRepository.java
@@ -10,7 +10,7 @@ public interface OrderRepository extends JpaRepository<Order, Integer> {
     @Query("FROM Order WHERE user_id = ?1 AND status = 0")
     Order getCustomerShopCart(int userId);
 
-    @Query("FROM Order WHERE status <> 0 ORDER BY ID DESC")
+    @Query("FROM Order WHERE user_id = ?1 AND status <> 0 ORDER BY ID DESC")
     List<Order> findAllByUser_IdOrderByIdDesc(int userId);
 
     @Query("FROM Order WHERE status <> 0 ORDER BY ID DESC")
